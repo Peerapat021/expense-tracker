8c2cfcdadd17167a51076676818b2cbbe26df680
Peerapat021 <149657882+Peerapat021@users.noreply.github.com>
2025-08-27 02:20:29 +0700

    เสร็จแล้ว

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,10 @@
-// client Supabase (เชื่อม DB)
-
 import { createClient } from '@supabase/supabase-js'
 
-export const supabaseServer = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY!
+// // log ตรวจสอบค่า env (ใช้เฉพาะตอน debug)
+// console.log("SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
+// console.log("SUPABASE_KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0,10) + "...")
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
